ceda9e56a4451356dac49aada2d89a93d8e167fa
Ayida.A <ayidamohamed@gmail.com>
2021-07-07 17:03:51 -0400

    day one

---
+++ JavaAss1/src/com/ss/jb/ass/Hello.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.ss.jb.ass;
+
+/**
+ * @author ayida
+ *
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+		for(String s : args)
+			System.out.println(s);
+
+	}
+
+}
